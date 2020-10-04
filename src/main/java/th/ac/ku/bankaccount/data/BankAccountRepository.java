27909d5eefc116566631dd04d8239f8416505896
6110406147 <phitchayatida.c@ku.th>
2020-10-04 22:14:56 +0700

    post mapping

---
+++ src/main/java/th/ac/ku/bankaccount/data/BankAccountRepository.java
@@ -0,0 +1,19 @@
+
+
+package th.ac.ku.bankaccount.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import th.ac.ku.bankaccount.model.BankAccount;
+
+import java.util.List;
+
+@Repository
+public interface BankAccountRepository extends
+        JpaRepository<BankAccount,Integer> {
+
+    List<BankAccount> findBycustomerId(int customerId);
+
+
+
+}
